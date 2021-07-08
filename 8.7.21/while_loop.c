2f374868d8e711b6a2bd3c7b32f967f12f508512
RAHUL VEESAM VEESAM <83008989+rahulveesam@users.noreply.github.com>
2021-07-08 12:45:14 +0000

    while

--- 8.7.21/while_loop.c
+++ 8.7.21/while_loop.c
@@ -3,14 +3,18 @@ int main(void) {
   int num1, num2, sum, count;
   count=0;
 
-  printf("enter number:\n");
-  scanf("%d %d", &num1, &num2);
-
   while(count<5)
   {
+    printf("count=%d\n", count);
+    printf("enter number:\n");
+    scanf("%d %d", &num1, &num2);
+
    sum=num1+num2;
    printf("%d+%d=%d\n", num1, num2, sum);
-   count++;
+   count+=1;
+
+   printf("times count executed : %d\n-----------\n", count);
+
   }
   return 0;
 }
